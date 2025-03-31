587a60051b132e84a7d91e89cb4f56822c45f76d
vvgviera <vvg.viera1971@gmail.com>
2025-03-31 09:16:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,46 @@ function closeModal() {
     document.getElementById('quizForm').focus();
 }
 
+function highlightAndSelectCorrect() {
+    // Selecciona todas las casillas de verificación tipo radio
+    const radioButtons = document.querySelectorAll('input[type="radio"]');
+    
+    radioButtons.forEach(radio => {
+        // Restablece el color de fondo de todas las casillas
+        radio.parentElement.style.backgroundColor = ""; 
+        
+        // Si el valor del radio button es "correct"
+        if (radio.value === "correct") {
+            // selecciona la casilla            
+            radio.checked = true; // Marca la casilla como seleccionada
+			return
+        }
+    });
+}
 
+// Llama a la función cuando sea necesario:
+//highlightAndSelectCorrect(); // Ejecutará los cambios al invocarla
+
+
+// Obtener referencia al elemento donde mostraremos el contador
+const visitCountElement = document.getElementById('visitCount');
+
+// Recuperar el contador de visitas desde el almacenamiento local
+let visitCount = localStorage.getItem('visitCount');
+
+// Si no existe un contador en el almacenamiento local, inicializarlo
+if (!visitCount) {
+    visitCount = 0;
+}
+
+// Incrementar el contador
+visitCount++;
+
+// Guardar el nuevo valor en el almacenamiento local
+localStorage.setItem('visitCount', visitCount);
+
+// Mostrar el contador en la página
+visitCountElement.textContent = visitCount;
 
 
 
